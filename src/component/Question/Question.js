f159c01832122435b491e02bb860dc98cd5e2075
Md-Mobassher <you@example.com>
2022-03-27 23:06:40 +0600

    Question added

---
+++ src/component/Question/Question.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Question = () => {
+    return (
+        <div>
+            <div>
+                <h4>Q. How react Works?</h4>
+                <p><b>Ans: </b> </p>
+            </div>
+            <div>
+                <h4>Q. Difference between Props and state?</h4>
+                <p><b>Ans: </b> </p>
+            </div>
+        </div>
+    );
+};
+
+export default Question;
